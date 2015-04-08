b31f1fa23299a14733844a0729a267eadaa01664
Tim Park <timfpark@gmail.com>
2015-04-08 14:49:37 -0700

    able to scrape page

--- server.js
+++ server.js
@@ -3,86 +3,87 @@ var async = require('async')
   , request = require('request')
   , cheerio = require('cheerio');
 
-var MIN_SEGMENT_ID = 229781;
-var MAX_SEGMENT_ID = 8000000;
-var WAIT_TIME_MS = 1000;
+var MIN_PEAK_ID = 1;
+var MAX_PEAK_ID = 60000;
+var WAIT_TIME_MS = 3000;
 
-var parseSegment = function(segmentId, callback) {
-	var segmentStreamUrl = 'http://www.strava.com/stream/segments/' + segmentId;
-	console.log('fetching json: ' + segmentStreamUrl);
+var parsePeak = function(peakId, callback) {
+	var peakHtmlUrl = 'http://peakbagger.com/peak.aspx?pid=' + peakId;
 
-	request(segmentStreamUrl, function(err, response, json) {
+	request(peakHtmlUrl, function(err, response, html) {
 		if (err) {
 			console.log(err);
-			return callback('stream fetch failed');
+			return callback('html fetch failed');
 		}
 
-		try {
-			console.log('parsing json');
-			var routeData = JSON.parse(json);
-		} catch(e) {
-			return callback(null, {
-				id: segmentId,
-				exists: false
-			})
+		var $ = cheerio.load(html);
+        var anchors = $('a');
+
+		console.log(peakId);
+
+		if ($('h1').text() === 'Invalid Peak ID')
+			return callback(null, { id: peakId, exists: false });
+
+		var elevationRaw = $('h2').text();
+
+		var elevationParsed = parseFloat(/\d+\+* meters/.exec(elevationRaw)[0]);
+
+        var latitude;
+        var longitude;
+
+        for (var key in anchors) {
+        	if (anchors[key].attribs && anchors[key].attribs.href) {
+	        	var locationUrl = anchors[key].attribs.href;
+	        	if (locationUrl && locationUrl.indexOf('maps.bing.com') !== -1) {
+	        		console.log(locationUrl);
+	        		var hrefSplit = locationUrl.split('=');
+	        		var locationSplit = hrefSplit[2].split('~');
+
+	        		latitude = parseFloat(locationSplit[0]);
+					longitude = parseFloat(locationSplit[1]);
+	        	}
+        	}
+        }
+
+		var peak = {
+			id: peakId,
+			exists: true,
+			name: $('h1').text(),
+
+			elevationRaw: $('h2').text(),
+			elevation: elevationParsed,
+
+			locationUrl: locationUrl,
+			latitude: latitude,
+			longitude: longitude
 		}
 
-		var segmentHtmlUrl = 'http://www.strava.com/segments/' + segmentId;
-		console.log('fetching html: ' + segmentHtmlUrl);
-
-		request(segmentHtmlUrl, function(err, response, html) {
-			if (err) {
-				console.log(err);
-				return callback('html fetch failed');
-			}
-
-			var $ = cheerio.load(html);
-
-			var segment = {
-				id: segmentId,
-				exists: true,
-				type: $('.location').children().first().text(),
-				name: $('.name').children().first().children().last().text(),
-				distance: $('.distance').children().first().text(),
-				lowestElevation: $('.elevation').children().first().text(),
-				highestElevation: $('.highest-elev').children().first().text(),
-				elevationChange: $('.elevation').next().next().children().first().text(),
-				attempts: $(".inline-stats").children().last().children().last().text().trim(),
-
-				latlng: routeData.latlng
-			}
-
-			return callback(null, segment);
-		});
+		return callback(null, peak);
 	});
 };
 
 async.forever(
 	function(next) {
-		console.log('waiting');
 		setTimeout(function() {
-			var segmentId;
+			var peakId;
+
 			do {
-				segmentId = Math.floor(Math.random() * (MAX_SEGMENT_ID - MIN_SEGMENT_ID) + MIN_SEGMENT_ID);
-				console.log('checking ' + segmentId);
-			} while (fs.existsSync('segments/' + segmentId));
+				peakId = Math.floor(Math.random() * (MAX_PEAK_ID - MIN_PEAK_ID) + MIN_PEAK_ID);
+			} while (fs.existsSync('peaks/' + peakId));
 
-			console.log('parsing ' + segmentId);
-
-			parseSegment(segmentId, function(err, segment) {
+			parsePeak(peakId, function(err, peak) {
 				if (err) return console.log('error: ' + err);
 
-				fs.writeFile('segments/' + segmentId, JSON.stringify(segment), function(err) {
+				fs.writeFile('peaks/' + peakId, JSON.stringify(peak), function(err) {
 					next();
 				});
 
-				if (segment.exists)
-					console.log(segment.id + ": " + segment.name + ": " + segment.latlng.length + ": " + segment.attempts);
+				if (peak.exists)
+					console.log(peak.id + ": " + peak.name + ": " + peak.latitude + ": " + peak.longitude + ": " + peak.elevation);
 				else
-					console.log(segment.id + ": nonexistant");
+					console.log(peak.id + ": nonexistant");
 			});
 		}, WAIT_TIME_MS * Math.random());
 	},
-
 	function(err) { }
 );
